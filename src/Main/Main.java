7477dcc583df104ed2a57441d99d8d2062049c0d
Your Name <you@example.com>
2024-12-05 17:57:23 +0300

    0512

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -16,24 +16,32 @@ public class Main {
         JFrame jFrame = getJFrame();
         JPanel jPanel = new JPanel();
         jFrame.add(jPanel);
-        JButton jButton = new JButton("СДАТЬ СЕССИЮ");
+        JButton jButton = new JButton("Вход");
+        jButton.setBounds(750,400, 600, 600);
+        jButton.setLocation(100,200);
+
         jPanel.add(jButton);
 
-        jButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    Administrator A = new Administrator("A", "2", "Weee", "W", 0);
-                } catch (Exception ex) {
-                    throw new RuntimeException(ex);
-                }
-            }
-        });
+//        jButton.addActionListener(new ActionListener() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                try {
+//                    Administrator A = new Administrator("A", "2", "Weee", "W", 0);
+//                } catch (Exception ex) {
+//                    throw new RuntimeException(ex);
+//                }
+//            }
+//        });
 
 
 
 
     }
+
+
+
+
+    //static methods-----------------------------------------------------------------------------------------------
 
     static JFrame getJFrame(){
         JFrame jFrame = new JFrame("University");
